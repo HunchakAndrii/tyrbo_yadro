556b685e27b24d69da0fbef371c8549c35f9c63d
HunchakAndrii <andreygunchak1@gmail.com>
2023-10-22 22:06:27 +0300

    finish all header

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,23 @@
 const burger = document.querySelector('.burger')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 if(burger) {
   burger.addEventListener('click', e => {
     e.preventDefault()
 
     burger.classList.toggle('active')
+    mobileMenu.classList.toggle('active')
   })
 }
+
+
+// document.querySelector('.burger').addEventListener('click', function() {
+//   var mobileMenu = document.querySelector('.mobile-menu');
+//   var burger = document.querySelector('.burger');
+  
+//   if (burger.style.display === 'active') {
+//     mobileMenu.style.display = 'active';
+//   } else {
+//     mobileMenu.style.display = 'none'
+//   }
+// });
