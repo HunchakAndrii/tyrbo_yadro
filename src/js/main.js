647cd720088966d3d59c1ec91486adb3bd1270d5
HunchakAndrii <andreygunchak1@gmail.com>
2023-10-30 16:28:52 +0200

    add broken slider and accordion

--- src/js/main.js
+++ src/js/main.js
@@ -47,7 +47,45 @@ if (btnClose && popup) {
   })
 }
 
+// .............slider.....................//
 
+// const swiper = new Swiper('.swiper', {
+//   direction: 'vertical',
+//   loop: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+
+var swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
+
+// var swiper = new Swiper('.mySwiper', {
+//   pagination: {
+//     el: '.swiper-pagination',
+//     type: 'fraction',
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
 
 
 
@@ -64,3 +102,63 @@ if (btnClose && popup) {
 // });
 
 
+//................acordion...................//
+
+console.log('Hello Cite88');
+
+
+// $(document).ready(function () {
+//   $('.accordion__item__title').click(function (even) {
+//     $(this).toggleClass('.active').next().slideToggle(300)
+//   })
+// })
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   var titles = document.querySelectorAll('.accordion__item__title')
+
+//   titles.forEach(function (title) {
+//     title.addEventListener('click', function (event) {
+//       this.classList.toggle('active')
+//       var content = this.nextElementSibling
+
+//       if (content.style.display === 'block') {
+//         content.style.display = 'none'
+//       } else {
+//         content.style.display = 'block'
+//       }
+//     })
+//   })
+// })
+
+
+// document.querySelectorAll('.accordion__item__title').forEach((item) =>
+//   item.addEventListener('click', () => {
+//     const parent = item.parentNode;
+
+//     if (parent.classList.contains('accordion__item--active')) {
+//       parent.classList.remove('accordion__item--active')
+//     } else {
+//       document
+//         .querySelectorAll('.accordion__item')
+//         .forEach((child) => child.classList.remove('accordion__item--active'))
+      
+      
+//         parent.classList.add('accordion__item--active')
+//     }
+//   })
+// )
+
+
+document.querySelectorAll('.accordion__item__title').forEach((el) => {
+  el.addEventListener('click', () => {
+
+    let content = el.nextElementSibling
+    console.log('test1');
+    
+
+  })
+})
+
+
+
+console.log('hfjkdshfvcsdl,kv');
